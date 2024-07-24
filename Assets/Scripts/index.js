a4b9c3eb2e2f3917fbed332636df19e567c9bb1e
Mike <124840291+mikewashereGH@users.noreply.github.com>
2024-07-24 11:36:38 -0400

    MainUpdate (v1)

---
+++ Assets/Scripts/index.js
@@ -0,0 +1,4 @@
+/*
+    This code is the intellectual property of the owner.
+    Unauthorized editing, removal, updating, adding, duplicating, selling, or reusing of this code is strictly prohibited.
+*/
